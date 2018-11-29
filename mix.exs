187fc2c49880ad1d1f394c89a5bff260febe8c66
Grigory Starinkin <starinkin@gmail.com>
2018-11-29 18:44:23 +0000

    tag as first stable release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixErllambda.MixProject do
   def project do
     [
       app: :mix_erllambda,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: "Elixir OTP release packaging for AWS Lambda",
